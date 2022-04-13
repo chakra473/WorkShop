3e0d8017b36980c7cce1d458e137983fec95c7e3
chakravarthy <m.chakra473@gmail.com>
2022-04-13 11:02:34 +0530

    ADDED ANAGRAM LOGIC

---
+++ Anagram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop
+{
+    internal class Anagram
+    {
+        public void anagramCheck()
+        {
+            Console.WriteLine("enter the string 1");
+            string string1=Console.ReadLine();
+            Console.WriteLine("enter the string 2");
+            string string2=Console.ReadLine();
+            char[] ch1=string1.ToLower().ToCharArray();
+            char[] ch2=string2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string res1=new string(ch1);
+            string res2=new string(ch2);
+            if(res1==res2)
+            {
+                Console.WriteLine("both the strings are ANAGRAM");
+            }
+            else
+            {
+                Console.WriteLine("both the strings are not ANAGRAM");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WorkShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ANAGRAM PROBLEM!");
+            Anagram anagram = new Anagram();
+            anagram.anagramCheck();
+        }
+    }
+}
